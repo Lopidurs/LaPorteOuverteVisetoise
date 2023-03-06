20b081843a0d5d6632611864b591f1cfbf67e5e7
lopidurs <nathan.sancke@hotmail.fr>
2023-03-06 13:52:45 +0100

    "CHORE(): Initialisation de eslint et prettier"
    Des règles elint et prettier ont été mit en place pour garantir
    l'uniformité du code.

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,14 @@
-
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-
-import './App.css';
-
-import Home from "./Pages/Home/Home.js"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './Pages/Home/Home.js'
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
--- front/src/Pages/Home/Home.test.js
+++ front/src/Pages/Home/Home.test.js
@@ -1,8 +1,8 @@
-import {render} from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Home from './Home'
 
 describe('Home', () => {
     it('Should render without crash', async () => {
-        render(<Home/>)
+        render(<Home />)
     })
 })
