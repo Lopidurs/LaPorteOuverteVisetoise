e4e5f03894312f1085ae0d82a72e60a42f3b97ee
Lopidurs <nathan.sancke@hotmail.fr>
2023-03-06 23:33:11 +0100

    FEAT(Games): Ajout tableau de list des jeux
    Ajout d'une nouvelle table principale "Games" qui référencie tous les jeux de la
    ludothèque ainsi que son affichage dans le frontend à l'aide d'un
    tableau mui pour gagner du temps.
    
    Issue: #4

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+
+import ListGames from './Pages/ListGames/ListGames.js'
 import Home from './Pages/Home/Home.js'
+
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/listGames" element={<ListGames />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ front/src/Pages/ListGames/ListGames.js
@@ -0,0 +1,77 @@
+import './ListGames.css'
+import { useState, useEffect } from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+import { StyledEngineProvider } from '@mui/material/styles'
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 69 },
+    { field: 'Name', headerName: 'Nom', width: 200 },
+    { field: 'Brand', headerName: 'Marque', width: 130 },
+    { field: 'Types', headerName: 'Types', width: 260 },
+    { field: 'Age', headerName: 'Age', width: 68 },
+    { field: 'MinPlayer', headerName: 'J min', width: 68 },
+    { field: 'MaxPlayer', headerName: 'J max', width: 68 },
+    { field: 'KeyWords', headerName: 'Mots clés', width: 260 },
+    { field: 'Release', headerName: 'Année', width: 68 },
+    { field: 'Awards', headerName: 'Prix', width: 210 },
+    { field: 'Status', headerName: 'Status', width: 100 }
+]
+
+function ListGames() {
+    const [ListGames, setListGames] = useState([])
+    function loadGames() {
+        fetch('http://localhost:3001/API/games', {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setListGames(data)
+            })
+    }
+
+    useEffect(() => {
+        loadGames()
+    }, [])
+
+    function formDate(date) {
+        try {
+            const dateArray = date.split('-')
+            return dateArray[0]
+        } catch {
+            return ''
+        }
+    }
+    const rows = ListGames.map((game) => {
+        return {
+            id: game.id,
+            Name: game.Name,
+            Brand: game.Brand,
+            Types: game.Types.map((type) => {
+                return type.Name
+            }).join(', '),
+            Age: game.Age + '+',
+            MinPlayer: game.MinPlayer,
+            MaxPlayer: game.MaxPlayer,
+            KeyWords: game.KeyWords.map((keyWord) => {
+                return keyWord.Name
+            }).join(', '),
+            Release: formDate(game.Release),
+            Awards: game.Awards.map((award) => {
+                return award.Name
+            }).join(', '),
+            Status: game.Status
+        }
+    })
+
+    return (
+        <div className="container">
+            <StyledEngineProvider injectFirst>
+                <div style={{ height: 400, width: '100%' }}>
+                    <DataGrid rows={rows} columns={columns} pageSize={1} rowsPerPageOptions={[5]} />
+                </div>
+            </StyledEngineProvider>
+        </div>
+    )
+}
+
+export default ListGames
