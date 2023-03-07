b65521624a83ab04e7c0907e8c9144cff1b9a1d8
Lopidurs <nathan.sancke@hotmail.fr>
2023-03-07 18:53:11 +0100

    test15

--- back/index.js
+++ back/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require('cors')
+require('dotenv').config();
 
 const env = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3001
