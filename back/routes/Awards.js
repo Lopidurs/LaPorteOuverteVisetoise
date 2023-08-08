7f86cb6552a588d944189def7cd337703826cbe7
Lopidurs <nathan.sancke@hotmail.fr>
2023-08-08 21:23:31 +0200

    FIX(login): authentification sur toutes les routes
    Le middleware d'authentification n'était présent que sur une seule route

--- back/routes/Awards.js
+++ back/routes/Awards.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const {Awards} = require("../models");
+const { Awards } = require("../models");
 
-router.get("/", async (req, res) => {
-    const listOfAwards = await Awards.findAll()
-    res.json(listOfAwards)
+router.get("/", authMiddleware, async (req, res) => {
+  const listOfAwards = await Awards.findAll()
+  res.json(listOfAwards)
 })
 
-router.post("/", async (req, res) => {
-    try {
-      const newAward = await Awards.create(req.body);
-      res.json(newAward);
-    } catch (error) {
-      res.status(400).send(error);
-    }
+router.post("/", authMiddleware, async (req, res) => {
+  try {
+    const newAward = await Awards.create(req.body);
+    res.json(newAward);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 })
 
 module.exports = router;
